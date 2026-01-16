d669a264aa6590a0c9831ff9ef3d398cacbd9db9
Vladislav Lyapunov <vlad.qvant@gmail.com>
2026-01-16 18:34:07 +0300

    0.21

---
+++ cwork 0.1/DEL.py
@@ -0,0 +1,2 @@
+car = 'audi'
+print(car == 'audi')
---
+++ cwork 0.1/if (начало).py
@@ -0,0 +1,13 @@
+car_list = ['vw', 'subasru', 'audi', 'lada', 'bmw'] 
+car = 'gazel'
+if car not in car_list:
+    print(f'Твоей машины нет в списке')
+car_1 = 'lotos'
+print(car_1.title() == 'Lotos')
+
+age_1 = 21
+age_2 = 15
+if age_1 >= 21 and age_2>14:
+    print('Это правда')
+else:
+    print('Это лож')
